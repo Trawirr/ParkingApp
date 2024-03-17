4b4343a88372842149a2cfe335dcedb0ee2fe4da
Trawirr <gtrawirr@gmail.com>
2024-03-17 10:50:54 +0100

    Api endpoint now enables to fetch certain parking data

--- src/Api/urls.py
+++ src/Api/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('hello-world/', views.hello_world, name='hello_world'),
-    path('parking_coords/', views.parking_coords, name='parking_coords'),
-    path('parking_slots_coords/', views.parking_slots_coords, name='parking_slots_coords'),
+    path('parking_coords/<int:parking_id>', views.parking_coords, name='parking_coords'),
+    path('parking_slots_coords/<int:parking_id>', views.parking_slots_coords, name='parking_slots_coords'),
 ]
--- src/Api/views.py
+++ src/Api/views.py
@@ -8,9 +8,9 @@ def hello_world(request):
     return Response({'message': 'It\'s a test paragraph'})
 
 @api_view(['GET'])
-def parking_coords(request):
-    return Response({'coords': [Parking.objects.all()[Parking.objects.all().count()-1].coords_list]})
+def parking_coords(request, parking_id: int):
+    return Response({'coords': [Parking.objects.get(id=parking_id).coords_list]})
 
 @api_view(['GET'])
-def parking_slots_coords(request):
-    return Response({'coords': Parking.objects.all()[Parking.objects.all().count()-1].slots_coords_list})
+def parking_slots_coords(request, parking_id: int):
+    return Response({'coords': Parking.objects.get(id=parking_id).slots_coords_list})
